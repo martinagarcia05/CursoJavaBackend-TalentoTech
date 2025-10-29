3a3cd7029bcccc92b27729ac5596a6a9a6fb0cca
martinagarcia05 <marti.garcia.amendola@gmail.com>
2025-10-29 20:56:41 -0300

    clase 10 spring boot

--- demo/src/main/java/com/techlab/demo/controller/EstudioController.java
+++ demo/src/main/java/com/techlab/demo/controller/EstudioController.java
@@ -1,11 +1,13 @@
 package com.techlab.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //debe controlar lo que entra desde afuera:
 @RestController
 public class EstudioController {
 //configuracion de los endpoints
+    @GetMapping("/hello")//localhost:8080 + "ruta que estamos configurando"
     public String helloWorld(){
         return "Hello World desde spring boot";
     }
