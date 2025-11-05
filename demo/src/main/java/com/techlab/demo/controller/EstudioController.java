87c8f4b0bfb6f73ba143c7b8fd9494fa0d9c0510
martinagarcia05 <marti.garcia.amendola@gmail.com>
2025-11-05 19:40:03 -0300

    clase 11

--- demo/src/main/java/com/techlab/demo/controller/EstudioController.java
+++ demo/src/main/java/com/techlab/demo/controller/EstudioController.java
@@ -1,7 +1,9 @@
-package com.techlab.demo.controller;
+package demo.src.main.java.com.techlab.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //debe controlar lo que entra desde afuera:
 @RestController
@@ -10,5 +12,18 @@ public class EstudioController {
     @GetMapping("/hello")//localhost:8080 + "ruta que estamos configurando"
     public String helloWorld(){
         return "Hello World desde spring boot";
+    }
+
+    //@PostMapping
+
+    //editarProducto
+    @PutMapping("/products/{id}") //put o patch es lo mismo en este caso
+    public String editarProducto(@PathVariable int id){
+        return "editando";
+    }
+
+    @GetMapping("/products/precio")
+    public List<String> buscarPordxPre(@RequestParam int precio){
+        return List.of("producto1");
     }
 }
